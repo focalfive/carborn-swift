e77b15d2c27d3d8c7d8052a0ca09e02dc722ef6e
pureye4u <pureye4u@gmail.com>
2019-01-15 02:10:11 +0900

    bind tableview

--- Carborn/ViewControllers/MainViewController.swift
+++ Carborn/ViewControllers/MainViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
+import SnapKit
 
 class MainViewController: UIViewController {
     internal var bag = DisposeBag()
+    internal lazy var tableView = UITableView(frame: .zero, style: .plain)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.center.equalTo(view)
+            $0.size.equalTo(view)
+        }
         
-        MenuService.shared.get().subscribe {
-            print("Subscribe RX menu", $0.element)
+        tableView.dataSource = nil
+        tableView.delegate = nil
+        
+        tableView.tableFooterView = UIView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MenuCell")
+        
+        MenuService.shared.get().bind(to: tableView.rx.items(cellIdentifier: "MenuCell", cellType: UITableViewCell.self)) { (row, element, cell) in
+            print(cell)
+            cell.textLabel?.text = element.name
         }.disposed(by: bag)
-        
     }
+    
+    
     
 }
